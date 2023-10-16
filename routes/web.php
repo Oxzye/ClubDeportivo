b94fa943a62d7834c76dd3486b1f79628d6f445e
Joaquin Calpanchay <joaquincalpanchay@gmail.com>
2023-10-15 21:26:46 -0300

    tablas crud gen y tdf

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\tipodetfacturaController;
+use App\Http\Controllers\GenerosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Rutas de Tipo de detalle de facturas
+Route::get('/Tipodetfactura', [tipodetfacturaController::class, 'index'])->name('tipos_detalle_factura.index');
+Route::post('/Tipodetfactura', [tipodetfacturaController::class, 'store'])->name('tipos_detalle_factura.store');
+Route::get('/Tipodetfactura/create', [tipodetfacturaController::class, 'create'])->name('tipos_detalle_factura.create');
+Route::put('/Tipodetfactura/{tdf}', [tipodetfacturaController::class, 'update'])->name('tipos_detalle_factura.update');
+Route::delete('/Tipodetfactura/{tdf}', [tipodetfacturaController::class, 'destroy'])->name('tipos_detalle_factura.destroy');
+Route::get('/Tipodetfactura/{tdf}/edit', [tipodetfacturaController::class, 'edit'])->name('tipos_detalle_factura.edit');
+
+//Rutas de Generos
+Route::get('/generos', [GenerosController::class, 'index'])->name('Generos.index');
+Route::post('/generos', [GenerosController::class, 'store'])->name('Generos.store');
+Route::get('/generos/create', [GenerosController::class, 'create'])->name('Generos.create');
+Route::put('/generos/{gen}', [GenerosController::class, 'update'])->name('Generos.update');
+Route::delete('/generos/{gen}', [GenerosController::class, 'destroy'])->name('Generos.destroy');
+Route::get('/generos/{gen}/edit', [GenerosController::class, 'edit'])->name('Generos.edit');
