126ab7b516d2feff17a0f66cc2fd7fbeb07ac55d
AldoMurilloG <aldomurillog@gmail.com>
2023-11-20 20:57:52 -0300

    CajasSaldos

--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -42,8 +42,8 @@
                                 <div class="info-box">
                                     <span class="info-box-icon bg-success"><i class="far fa-flag"></i></span>
                                     <div class="info-box-content">
-                                        <span class="info-box-text">Bookmarks</span>
-                                        <span class="info-box-number">410</span>
+                                        <span class="info-box-text">Saldo de cajas</span>
+                                        <span class="info-box-number">{{ $saldo_cajas }}</span>
                                     </div>
 
                                 </div>
