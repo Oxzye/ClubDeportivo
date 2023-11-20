e5a5445480f8faecb88dfb3bc3fda04670aefe97
AldoMurilloG <aldomurillog@gmail.com>
2023-11-20 20:33:57 -0300

    Agregado de ChartJS

--- routes/panel.php
+++ routes/panel.php
@@ -78,6 +78,7 @@ Route::get('/exportar-diasxact-excel', [DiasxActController::class, 'exportarDias
 
 Route::resource('/SocxAct', SociosxActividadesController::class)->names('SocxAct');
 Route::get('/exportar-socxact-excel', [SociosxActividadesController::class, 'exportarSocxActExcel'])->name('exportar-socxact-excel');
+Route::get('graficos-socxact',[SociosxActividadesController::class,'graficosSocxAct'])->name('graficos-socxact');
 
 Route::resource('/EmpxAct', EmpleadosxActividadesController::class)->names('EmpxAct');
 
